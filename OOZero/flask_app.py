cfb927d3f77b31f4a322bdf0c4c5b274bea93b25
Jared Butcher <jared.butcher1219@gmail.com>
2019-11-24 17:30:08 -0500

    edit user working proably

--- OOZero/flask_app.py
+++ OOZero/flask_app.py
@@ -1,6 +1,6 @@
 from OOZero import create_app
 from flask import Flask, render_template, request, redirect, url_for, session, flash
-from OOZero.user_model import authenticateUser, addUser, hashPassword, getUser
+from OOZero.user_model import authenticateUser, addUser, hashPassword, getUser, editUser
 from OOZero.user_session import login_required, user_login, user_logout
 from OOZero.event_blueprint import events
 
@@ -51,10 +51,41 @@ def signup():
         if check:
             error = 'Username already taken.'
         else:
-            addUser(username=username, password=password, name=name, email=email)
-            session['username'] = username
-            return redirect('/')
+            try:
+                addUser(username=username, password=password, name=name, email=email)
+                session['username'] = username
+                return redirect('/')
+            except (ValueError, TypeError) as e:
+                error = str(e)
     return render_template('signup.html', error=error)
+
+@app.route('/manage_user/', methods=['POST', 'GET'])
+@login_required
+def manage_user():
+    error = None
+    user=getUser(session["username"])
+    if request.method == 'POST':
+        username, email, name, newPassword = (None,)*4
+
+        if hashPassword(request.form['currentPassword'], user.salt) != user.password_hash:
+            error = 'Password is incorrect.'
+        if not request.form['username'] is None and request.form['username'] != '' and request.form['username'] != user.username:
+            if getUser(request.form['username']) is not None:
+                error = 'Username already taken.'
+            username = request.form['username']
+        if request.form['email'] != '' and request.form['email'] != user.email:
+            email = request.form['email']
+        if request.form['name'] != '' and request.form['name'] != user.name:
+            name = request.form['name']
+        if request.form['newPassword'] != '':
+            if len(request.form['newPassword']) < 4:
+                error = "Password must be at least 4 charactors"
+            newPassword = request.form['newPassword']
+
+        if error is None:
+            user = editUser(user, username=username, password=newPassword, name=name, email=email)
+            session['username'] = user.username
+    return render_template('manage_user.html', error=error, user=user)
 
 @app.route('/logout/', methods=['POST', 'GET'])
 def logout():
